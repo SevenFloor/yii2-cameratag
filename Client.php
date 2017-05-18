f4166485d50913d870a51be29887bef6ca754636
Ignatenkov Nikita <g.kiting@gmail.com>
2017-05-18 19:03:04 +0300

    component

---
+++ Client.php
@@ -0,0 +1,13 @@
+<?php
+namespace sevenfloor\cameratag;
+
+use yii\base\Configurable;
+
+/**
+ * Class Client
+ * @package sevenfloor\cameratag
+ */
+class Client extends \cameratag\Client implements Configurable
+{
+
+}
